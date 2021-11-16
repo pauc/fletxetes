a2d4c7e2458ab85c61129b597751632731f0f251
Pau Compte <pausandalio@pm.me>
2021-11-17 00:12:49 +0100

    Add a very basic test for `Fletxetes::Repos::Shots#update`

--- spec/suite/application/fletxetes/repos/shots_spec.rb
+++ spec/suite/application/fletxetes/repos/shots_spec.rb
@@ -2,11 +2,10 @@
 
 RSpec.describe Fletxetes::Repos::Shots, :db do
   subject(:repo) { described_class.new }
+  let(:batch) { repo.batches.command(:create).call({}) }
 
   describe "#create" do
     it "accepts the position as a hash" do
-      batch = repo.batches.command(:create).call({})
-
       shot_attrs = {
         batch_id: batch.id,
         number: 1,
@@ -34,6 +33,24 @@ RSpec.describe Fletxetes::Repos::Shots, :db do
 
       expect(created_shot.id).not_to be_nil
       expect(created_shot.position).to be_nil
+    end
+  end
+
+  describe "#update" do
+    it "works" do
+      shot_attrs = {
+        batch_id: batch.id,
+        number: 1,
+        punctuation: 6,
+        position: { x: 3.14, y: -1.29 }
+      }
+
+      shot = repo.create(shot_attrs)
+
+      expect { repo.update(shot.id, punctuation: 8) }
+        .to change { repo.shots.by_pk(shot.id).one.punctuation }
+        .from(6)
+        .to(8)
     end
   end
 end
