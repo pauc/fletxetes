a98566ab22c46e703b93679baf89363c88576425
Pau Compte <pausandalio@pm.me>
2021-11-17 00:13:19 +0100

    Add more attributes to `batches`

--- db/migrate/20211028212631_create_batches.rb
+++ db/migrate/20211028212631_create_batches.rb
@@ -3,9 +3,12 @@
 ROM::SQL.migration do
   change do
     create_table(:batches) do
-      primary_key :id, type: :Bignum
-      column :distance, Float
-      column :created_at, DateTime, null: false
+      primary_key :id,          type: :Bignum
+      column      :distance,    Float
+      column      :punctuation, Integer,  null: false, default: 0
+      column      :max_shots,   Integer,  null: false, default: 0
+      column      :shots_count, Integer,  null: false, default: 0
+      column      :created_at,  DateTime, null: false
     end
   end
 end
